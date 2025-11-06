d3984c95f1012a05036a25ff6bbe8777ef01762e
aphexya <ahmadafhaassalamiii@gmail.com>
2025-11-06 16:03:17 +0700

    Commit ke-5

--- test.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-if (fs.existsSync('index.html')) {
-    console.log("✅ File index.html ditemukan!");
-    process.exit(0);
-} else {
-    console.error("❌ File index.html tidak ditemukan!");
-    process.exit(1);
-}
-
